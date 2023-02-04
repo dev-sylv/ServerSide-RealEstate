54da1f6c7962ed9301e35e417bd78273777b3593
nicsylvia <nicsylvia15f@gmail.com>
2023-02-03 20:15:55 -0800

    saved all again

--- app.ts
+++ app.ts
@@ -8,22 +8,23 @@ import { ErrorHandler } from "./MiddleWares/ErrorHandling/ErrorHandler";
 
 export const AppConfig = (app: Application) =>{
     // Configuring middlewares
+    app.use(express.json());
     app.use(cors());
     app.use(morgan("dev"));
-    app.use(express.json());
+    
 
     // configuring my routes for my application
     app.use("/api/agents", agentrouter);
     app.use("/api/houses", houserouter)
 
-    app.all("*", (req: Request, res: Response, next: NextFunction) =>{
-        next(
-            new AppError({
-                message: `This route ${req.originalUrl} does not exist`,
-                httpCode: HttpCode.NOT_FOUND
-            })
-        )
-    });
+    // app.all("*", (req: Request, res: Response, next: NextFunction) =>{
+    //     next(
+    //         new AppError({
+    //             message: `This route ${req.originalUrl} does not exist`,
+    //             httpCode: HttpCode.NOT_FOUND
+    //         })
+    //     )
+    // });
 
-    app.use(ErrorHandler)
+    // app.use(ErrorHandler)
 }
