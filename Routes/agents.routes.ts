d882163969bcf66e9c86cb746ee007975347e09c
nicsylvia <nicsylvia15f@gmail.com>
2023-02-03 18:03:24 -0800

    saved agents routes

---
+++ Routes/agents.routes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { AgentsLogin, GetAgents, OneAgent, RegisterAgents } from "../Controller/agents.controller";
+import { LoginValidation, RegisterValidation } from "../MiddleWares/Validation/AgentValidation/AgentValidation";
+
+const router = express.Router();
+
+router.route("/getallagents").get(GetAgents);
+router.route("/getoneagent").get(OneAgent);
+router.route("/registeragents").post(RegisterValidation, RegisterAgents)
+router.route("/loginagent").post(LoginValidation, AgentsLogin);
+
+export default router
--- app.ts
+++ app.ts
@@ -1,6 +1,9 @@
-import express, { Application } from "express";
+import express, { Application, Response, Request, NextFunction } from "express";
 import cors from "cors";
 import morgan from "morgan"
+import router from "./Routes/agents.routes";
+import { AppError, HttpCode } from "./Utils/AppError";
+import { ErrorHandler } from "./MiddleWares/ErrorHandling/ErrorHandler";
 
 export const AppConfig = (app: Application) =>{
     // Configuring middlewares
@@ -9,4 +12,16 @@ export const AppConfig = (app: Application) =>{
     app.use(express.json());
 
     // configuring my routes for my application
+    app.use("/api/agents", router)
+
+    app.all("*", (req: Request, res: Response, next: NextFunction) =>{
+        next(
+            new AppError({
+                message: `This route ${req.originalUrl} does not exist`,
+                httpCode: HttpCode.NOT_FOUND
+            })
+        )
+    });
+
+    app.use(ErrorHandler)
 }
