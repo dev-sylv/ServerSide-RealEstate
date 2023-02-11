28f3e138f2d004cf8311dbc4160964550815346c
nicsylvia <nicsylvia15f@gmail.com>
2023-02-11 11:31:11 -0800

    saved configured local URI

--- Config/DB.ts
+++ Config/DB.ts
@@ -2,9 +2,11 @@ import mongoose from "mongoose";
 
 const DB_URL = "mongodb+srv://sylviaDB:devsylvia@cluster0.fhx2vt1.mongodb.net/REALESTATEPROJECT?retryWrites=true&w=majority"
 
+const Local_URI = "mongodb://localhost/REALESTATEPROJECT"
+
 export const DBconnect = async() =>{
     try {
-        const connect = await mongoose.connect(DB_URL);
+        const connect = await mongoose.connect(Local_URI);
         console.log(`Database is connected to ${connect.connection.host}`)
     } catch (error) {
         console.log("An error occured in connecting database", error)
